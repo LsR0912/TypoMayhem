3180cb2f8de6890a1be122e16c8feb59d279e93d
laser <laser0912@gmail.com>
2025-03-24 07:35:51 +0100

    Implemented INotifyPropertyChanged interface

--- TypoMayhem/ViewModel/TypingViewModel.cs
+++ TypoMayhem/ViewModel/TypingViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TypoMayhem.ViewModel
 {
-	public class TypingViewModel
+	public class TypingViewModel : INotifyPropertyChanged
 	{
+		public event PropertyChangedEventHandler? PropertyChanged;
 	}
 }
