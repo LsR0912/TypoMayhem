8a1ada8edd6824188d390f17afcb675e800b0519
laser <laser0912@gmail.com>
2025-03-27 13:24:29 +0100

    Added UserStatistic properties

--- TypoMayhem/Model/UserStatistic.cs
+++ TypoMayhem/Model/UserStatistic.cs
@@ -8,5 +8,12 @@ namespace TypoMayhem.Model
 {
 	public class UserStatistic
 	{
+		// Properties
+		public int Id { get; set; }
+		public string? Username { get; set; }
+		public int SessionDuration { get; set; }
+		public int ErrorCount { get; set; }
+		public double WordsPerMinute { get; set; }
+		public double SignsPerMinute { get; set; }
 	}
 }
