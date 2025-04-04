6219271a99d34053c132eeddc4a93ecaa8a230c9
laser <laser0912@gmail.com>
2025-04-04 09:09:24 +0200

    Created TypingCourse class

---
+++ TypoMayhem/Model/TypingCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypoMayhem.Model
+{
+	public class TypingCourse
+	{
+		public string? CourseName { get; set; }
+		public string? CourseText { get; set; }
+	}
+}
