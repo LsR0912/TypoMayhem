708f6fb119c9d00720caae91b5179dbcd80ff624
Dennis <laser0912@gmail.com>
2025-04-09 10:58:03 +0200

    Add variables and properties

--- TypoMayhem/ViewModel/EvaluationViewModel.cs
+++ TypoMayhem/ViewModel/EvaluationViewModel.cs
@@ -10,8 +10,20 @@ namespace TypoMayhem.ViewModel
 {
 	public class EvaluationViewModel : INotifyPropertyChanged
 	{
+		// Variables
+		private int _totalSigns;
+		private int _totalErrors;
+		private double _avgWPM;
+
+		// Properties
+		public int TotalSigns { get => _totalSigns; set => SetProperty(ref _totalSigns, value); }
+		public int TotalErrors { get => _totalErrors; set => SetProperty(ref _totalErrors, value); }
+		public double AvgWPM { get => _avgWPM; set => SetProperty(ref _avgWPM, value); }
+
+		// Events
 		public event PropertyChangedEventHandler? PropertyChanged;
 
+		// Methods
 		protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
